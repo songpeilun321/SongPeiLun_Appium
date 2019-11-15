ef6a1fecc8b76d102886db171a02be0de2301caf
songpeilun <songpeilun321@163.com>
2019-11-15 10:53:29 +0800

    优化操作定位元素

--- operation/operation_click.py
+++ operation/operation_click.py
@@ -6,17 +6,34 @@
 # @File    : operation_click.py
 # @Software: PyCharm
 #对元素进行操作
-from test.login_page import LoginPage
 import time
+
+from page.login_page import LoginPage
+
 
 class Click_page:
     def __init__(self):
         self.lopage = LoginPage()
 
-
+    #操作文件元素
     def click_resource_file(self):
         time.sleep(2)
         self.lopage.get_page_resource_file_element().click()
+
+    #操作通话元素元素
+    def click_resource_iphone(self):
+        time.sleep(2)
+        self.lopage.get_page_resource_iphone_element().click()
+
+    #操作软件协议同意元素
+    def click_resource_consent(self):
+        time.sleep(2)
+        self.lopage.get_page_resource_consent_element().click()
+
+    #操作密码登录元素
+    def click_resource_login(self):
+        time.sleep(2)
+        self.lopage.get_page_resource_login_element().click()
 
 
 if __name__ == '__main__':
